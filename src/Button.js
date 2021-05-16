ceeb19fbf141a2cea28cb1f2bf141ad02706ad2b
Lavinia Loredana <lavinialoreda@gmail.com>
2021-05-16 21:12:26 +0200

    6 - Installed Bootstrap

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ btnText }) => {
-    return <a> {btnText} </a>;
+    return <a className="btn btn-info"> {btnText} </a>;
 };
 
 export default Button;
