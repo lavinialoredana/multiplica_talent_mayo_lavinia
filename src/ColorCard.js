a5c285af748b7403065bb1f5c9198ee9fe137df3
Lavinia Loredana <lavinialoreda@gmail.com>
2021-05-16 20:45:13 +0200

    4 -Created the click to copy message and styles

--- src/ColorCard.js
+++ src/ColorCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 const ColorCard = ({ year, name, color, pantoneValue }) => {
     return (
         <div className="color-card-container border" style={{ backgroundColor: color }}>
@@ -8,9 +7,9 @@ const ColorCard = ({ year, name, color, pantoneValue }) => {
                 <p> {name} </p>
                 <h3> {color} </h3>
             </div>
+            <h2 className="message-on-hover"> Click to copy</h2>
             <p>{pantoneValue}</p>
         </div>
     );
 };
-
 export default ColorCard;
